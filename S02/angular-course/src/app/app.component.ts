86182adedc3861470e2c899be71562dfe7da7a42
Oliver <matthrews@outlook.com>
2023-03-20 20:09:58 +0800

    pipe syntax 3

--- S02/angular-course/src/app/app.component.ts
+++ S02/angular-course/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { COURSES } from '../db-data';
 })
 export class AppComponent {
     courses = COURSES;
+    course = COURSES[1];
     title = COURSES[1].description;
     price = 9.99345667;
     startDate = new Date(2000, 0, 1);
